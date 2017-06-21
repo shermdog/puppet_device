f06b5586569eafbec6d3983728c70043a74c45d4
tkishel <tkishel@gmail.com>
2017-06-21 15:00:50 -0700

    implement inifile

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -2,19 +2,5 @@
 # @api private
 
 class puppet_device::conf {
-  File {
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
-  }
-
   $device_conf = $::puppet_deviceconfig
-  ini_setting { "puppet_device_conf [comment]":
-    ensure            => present,
-    key_val_separator => ' ',
-    section           => '',
-    setting           => 'comment',
-    value             => 'This file is managed by the puppet_device module',
-    path              => $puppet_device::conf::device_conf,
-  }
 }
--- manifests/conf/device.pp
+++ manifests/conf/device.pp
@@ -7,23 +7,21 @@ define puppet_device::conf::device (
   Enum['present', 'absent'] $ensure = 'present',
 ) {
   include puppet_device::conf
-  if ($ensure == 'present') {
-    ini_setting { "puppet_device_conf [${title}] type":
-      ensure            => present,
-      key_val_separator => ' ',
-      section           => $title,
-      setting           => 'type',
-      value             => $type,
-      path              => $puppet_device::conf::device_conf,
-    }
-    ini_setting { "puppet_device_conf [${title}] type":
-      ensure            => present,
-      key_val_separator => ' ',
-      section           => $title,
-      setting           => 'url',
-      value             => $url,
-      path              => $puppet_device::conf::device_conf,
-    }
+  ini_setting { "puppet_deviceconfig [${title}] type":
+    ensure            => $ensure,
+    key_val_separator => ' ',
+    section           => $title,
+    setting           => 'type',
+    value             => $type,
+    path              => $puppet_device::conf::device_conf,
+  }
+  ini_setting { "puppet_deviceconfig [${title}] url":
+    ensure            => $ensure,
+    key_val_separator => ' ',
+    section           => $title,
+    setting           => 'url',
+    value             => $url,
+    path              => $puppet_device::conf::device_conf,
   }
 }
 
